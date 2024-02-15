353c04f8e5b272b3a68bd4aaeb5b13336e3373bc
RayaSerahill <ville@daini.dev>
2024-02-15 11:22:19 +0200

    Proper setup to give another class the instance of the plugin

--- src/main/java/net/serahill/minereignItems/MinereignItems.java
+++ src/main/java/net/serahill/minereignItems/MinereignItems.java
@@ -4,9 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinereignItems extends JavaPlugin {
 
+    public static MinereignItems plugin;
+    private CraftingRecipeManager RecipeManager;
     @Override
     public void onEnable() {
+        plugin = this;
+
         this.saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
+
+        this.RecipeManager = new CraftingRecipeManager(this);
     }
 }
